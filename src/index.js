1373bb515d087ecaec92f653c0fe141eb6fe6b8d
dy0yb <15639927207@sina.cn>
2019-12-21 23:14:14 +0800

    fleat: 优雅地修改共享状态

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// 数据
+const appState = {
+    title: {
+        text: 'Reat.js 小书',
+        color: 'red',
+    },
+    content: {
+        text: 'React.js 小书内容',
+        color: 'blue',
+    }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// 渲染函数，吧状态的数据渲染到页面上。
+function renderApp (appState) {
+    renderTitle(appState.title)
+    renderContent(appState.content)
+}
+function renderTitle (title) {
+    const titleDOM = document.getElementById('title')
+    titleDOM.innerHTML = title.text
+    titleDOM.style.color = title.color
+}
+
+// 修改appState
+function dispatch (action) {
+    switch (action.type) {
+        case 'UPDATE_TITLE_TEXT':
+            appState.title.text = action.text
+            break
+        case 'UPDATE_TITLE_COLOR':
+            appState.title.color = action.color
+            break
+        default:
+            break
+    }
+}
+
+function renderContent (content) {
+    const contentDOM = document.getElementById('content')
+    contentDOM.innerHTML = content.text
+    contentDOM.style.color = content.color
+}
+
+
+dispatch({type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》'})
+renderApp(appState);
+
