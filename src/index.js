dac4e0784584c7252f8cacacc4a13c2d54754a16
dy0yb <15639927207@sina.cn>
2019-12-22 00:17:39 +0800

    fealt: 抽离store和监控数据变化

--- src/index.js
+++ src/index.js
@@ -21,21 +21,6 @@ function renderTitle (title) {
     titleDOM.innerHTML = title.text
     titleDOM.style.color = title.color
 }
-
-// 修改appState
-function dispatch (action) {
-    switch (action.type) {
-        case 'UPDATE_TITLE_TEXT':
-            appState.title.text = action.text
-            break
-        case 'UPDATE_TITLE_COLOR':
-            appState.title.color = action.color
-            break
-        default:
-            break
-    }
-}
-
 function renderContent (content) {
     const contentDOM = document.getElementById('content')
     contentDOM.innerHTML = content.text
@@ -43,6 +28,37 @@ function renderContent (content) {
 }
 
 
-dispatch({type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》'})
-renderApp(appState);
+// 修改appState
+function stateChanger  (state, action) {
+    switch (action.type) {
+        case 'UPDATE_TITLE_TEXT':
+            state.title.text = action.text
+            break
+        case 'UPDATE_TITLE_COLOR':
+            state.title.color = action.color
+            break
+        default:
+            break
+    }
+} 
+
+
+function createStore (state, stateChanger) {
+    const listeners = []
+    const subscribe = (listener) => listeners.push(listener)
+
+    const getState = () => state
+    const dispatch = (action) => {
+        stateChanger(state, action)
+        listeners.forEach((listener)=>listener())
+    }
+    return { getState, dispatch, subscribe}
+}
+
+const store = createStore(appState, stateChanger)
+store.subscribe(()=>renderApp(store.getState()))
+
+renderApp(store.getState());
+store.dispatch({type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》'})
+store.dispatch({type: 'UPDATE_TITLE_COLOR', color: 'blue'})
 
