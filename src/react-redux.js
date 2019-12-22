25f3504a8d2d06e941511e5ca43e29256807bc80
dy0yb <15639927207@sina.cn>
2019-12-23 00:08:31 +0800

    fleat: mapStateToProps

--- src/react-redux.js
+++ src/react-redux.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-export const connect = (mapStateToProps) => (WrappedComponent) => {
+export const connect = (mapStateToProps, mapDispatchToProps) => (WrappedComponent) => {
     class Connect extends Component {
         static contextTypes = {
             store: PropTypes.object
         }
-        
-        render () {
-            const { store } = this.context
-            let stateProps = mapStateToProps(store.getState())
 
-            return <WrappedComponent {...stateProps}></WrappedComponent>
+        constructor () {
+            super()
+            this.state = { allProps: {} }
+        }
+        
+        componentWillMount() {
+            const { store } = this.context
+            this._updateProps()
+            store.subscribe(()=>this._updateProps())
+        }
+
+        _updateProps () {
+            const { store } = this.context
+            let stateProps = mapStateToProps
+            ? mapStateToProps(store.getState(), this.props):{}
+            let dispatchProps = mapDispatchToProps
+            ? mapDispatchToProps(store.dispatch, this.props):{}
+            this.setState({
+                allProps:{
+                    ...stateProps,
+                    ...dispatchProps,
+                    ...this.props
+                }
+            })
+        }
+
+        render () {
+            return <WrappedComponent {...this.state.allProps}></WrappedComponent>
         }
     }
 
